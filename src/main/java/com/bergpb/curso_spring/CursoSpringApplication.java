675e62157eeab4842277fcb8b6ce35b5aeb3547b
Berg Paulo <lindembergdepaulo@gmail.com>
2018-04-21 23:25:15 -0300

    projeto criado

---
+++ src/main/java/com/bergpb/curso_spring/CursoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.bergpb.curso_spring;
+
+//classe principal (main)
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+}
